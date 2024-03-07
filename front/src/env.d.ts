b3b312a8a6ec417fb1882b322119d68c2fc28391
viacheslav <viacheslavorlov@gmail.com>
2024-03-08 00:14:31 +0300

    strapi as cms started

---
+++ front/src/env.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+/// <reference types="astro/astro-jsx" />
+
+interface ImportMetaEnv {
+	readonly STRAPI_URL: string;
+}
